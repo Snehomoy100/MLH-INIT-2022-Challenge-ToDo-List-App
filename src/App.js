cded8a8218726555a30fe8ffa0c2d11fa2829538
snehomoy100 <snehomoy100@gmail.com>
2021-06-29 19:31:58 +0530

    App.css file updated with the custom made CSS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <p>Nicee</p>
+    <div>
+      <p>Nicee App</p>
     </div>
   );
 }
