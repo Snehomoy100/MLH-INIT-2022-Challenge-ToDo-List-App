bb6cb265e89c0af6a5e2e288a7a391cb9b6b0df3
snehomoy100 <snehomoy100@gmail.com>
2021-06-29 22:07:26 +0530

    Delete ToDos feature added

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,17 +1,34 @@
 import React from 'react';
 
 const TodoList = ({todos, setTodos}) => {
+
+    const handleDelete = ({id}) => {
+        setTodos(todos.filter((todo) => todo.id !== id));
+    };
+
+
     return (
         <div>
             {todos.map((todo) => {
                 return(
-                <li className="todo-list" key={todo.id}>
+                <li className="list-item" key={todo.id}>
                     <input 
                         type="text" 
                         value={todo.title} 
                         className="list" 
                         onChange={(event) => event.preventDefault()}>
                     </input>
+                    <div>
+                        <button className="button-complete task-button" onClick={() => handleDelete}>
+                            <i className="fa fa-check-circle"></i>
+                        </button>
+                        <button className="button-edit task-button">
+                            <i className="fa fa-edit"></i>
+                        </button>
+                        <button className="button-delete task-button" onClick={() => handleDelete(todo)}>
+                            <i className="fa fa-trash"></i>
+                        </button>
+                    </div>
                 </li>
                 )
             })}
